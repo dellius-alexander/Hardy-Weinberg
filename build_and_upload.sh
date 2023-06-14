fa6230369608f21b7b02a2449d42bb0dc7d642a3
dellius-alexander <dellius.alexander@gmail.com>
2023-06-14 01:24:38 -0400

    reformated with black, running gh actions to build test and deploy

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-VERSION="0.2.8"
+VERSION="0.2.9"
 rm -rf dist/** && \
 sed -e "s/version = \"[[:digit:]]\.[[:digit:]]\.[[:digit:]]\"/version = \"${VERSION}\"/g" setup.py | tee setup.temp.py && \
 sed -e "s/version = \"[[:digit:]]\.[[:digit:]]\.[[:digit:]]\"/version = \"${VERSION}\"/g" setup.cfg | tee setup.temp.cfg && \
-sed -e "s/version = \"[[:digit:]]\.[[:digit:]]\.[[:digit:]]\"/version = \"${VERSION}\"/g" pyproject.toml | tee pyproject.temp.toml && \
 mv setup.temp.py setup.py && \
 mv setup.temp.cfg setup.cfg && \
 mv pyproject.temp.toml pyproject.toml
@@ -15,12 +14,10 @@ else
     exit 1
 fi
 
-python3 -m twine upload dist/* --verbose --skip-existing
-
-if python3 -m pip install hardyweinbergcalculator 2>&1 ; then
-    echo "Successfully installed the package"
-    python3 -m hwc --help
-    python3 -m hwc --version
-else
-    echo "Failed to install the package"
-fi
+#python3 -m twine upload dist/* --verbose --skip-existing
+#python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose --skip-existing
+#if python3 -m pip install hardyweinbergcalculator 2>&1 ; then
+#    echo "Successfully installed the package"
+#else
+#    echo "Failed to install the package"
+#fi
